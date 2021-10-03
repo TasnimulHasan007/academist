2b7b7765a9b2dbe3725019c370c7855eb9633746
Tasnimul Hasan <thtanim2005@gmail.com>
2021-10-04 00:09:28 +0600

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Home from "./components/Home/Home"
@@ -6,6 +5,7 @@ import NotFound from "./components/NotFound/NotFound"
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
 import Services from "./components/Services/Services"
+import "./App.css"
 
 function App() {
   return (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,46 @@
 import React from "react"
 import { Container } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
+import "./Header.css"
 
 const Header = () => {
   return (
     <header>
       <Container>
-        <h1>header</h1>
+        <nav>
+          {/* brand */}
+          <div>
+            <NavLink to="/home" className="brand">
+              <i class="fas fa-graduation-cap"></i> Academist
+            </NavLink>
+          </div>
+          {/* nav links */}
+          <ul className="nav-links">
+            <li>
+              <NavLink className="nav-link" activeClassName="active" to="/home">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                to="/services"
+              >
+                Services
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                to="/about"
+              >
+                About Us
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </Container>
     </header>
   )
