7e28b2b41095ba840c82b929837e40a26e9b3a18
Tasnimul Hasan <thtanim2005@gmail.com>
2021-10-04 07:00:24 +0600

    home page hero section added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,48 +9,68 @@ const Home = (props) => {
   const courses = props.courses.slice(0, 4)
   // return
   return (
-    <Container>
-      <Row className="my-5">
-        <Col md={6} s={12}>
-          <div className="d-flex justify-content-between align-items-center featured-header">
-            <h3>SERVICES WE OFFER</h3>
-            <Link to="/services" className="main-btn">
-              See all
+    <div>
+      <div className="hero">
+        <Container>
+          <Col md={6}>
+            <h2>Education even from far away</h2>
+            <p>
+              Academist is an online learning and teaching marketplace with over
+              155000 courses and 40 million students. Learn programming,
+              marketing, data science and more.
+            </p>
+            <Link className="main-btn" to="/about">
+              About Us
             </Link>
-          </div>
-          <Row className="featured-courses g-4 mx-5 mt-2">
-            {courses.map((course) => (
-              <FeaturedCourse key={course.key} course={course}></FeaturedCourse>
-            ))}
-          </Row>
-        </Col>
-        <Col
-          md={6}
-          s={12}
-          className="form d-flex flex-column justify-content-center"
-        >
-          <div className="d-flex justify-content-between align-items-center pb-4">
-            <h4>BOOK A FREE LESSON</h4>
-            <i className="fas fa-pencil-alt"></i>
-          </div>
-          <div className="inputs">
-            <div className="input-container">
-              <i className="fas fa-user"></i>
-              <input type="text" placeholder="Name" required />
+          </Col>
+        </Container>
+      </div>
+      <Container>
+        <Row className="my-5">
+          <Col lg={6} md={12}>
+            <div className="d-flex justify-content-between align-items-center featured-header">
+              <h3>SERVICES WE OFFER</h3>
+              <Link to="/services" className="main-btn">
+                See all
+              </Link>
             </div>
-            <div className="input-container">
-              <i className="fas fa-phone"></i>
-              <input type="tel" placeholder="Phone" required />
+            <Row className="featured-courses g-4 mx-5 my-3">
+              {courses.map((course) => (
+                <FeaturedCourse
+                  key={course.key}
+                  course={course}
+                ></FeaturedCourse>
+              ))}
+            </Row>
+          </Col>
+          <Col
+            lg={6}
+            md={12}
+            className="form d-flex flex-column justify-content-center"
+          >
+            <div className="d-flex justify-content-between align-items-center pb-4">
+              <h4>BOOK A FREE LESSON</h4>
+              <i className="fas fa-pencil-alt"></i>
             </div>
-            <div className="input-container">
-              <i className="fas fa-envelope"></i>
-              <input type="email" placeholder="Email" required />
+            <div className="inputs">
+              <div className="input-container">
+                <i className="fas fa-user"></i>
+                <input type="text" placeholder="Name" required />
+              </div>
+              <div className="input-container">
+                <i className="fas fa-phone"></i>
+                <input type="tel" placeholder="Phone" required />
+              </div>
+              <div className="input-container">
+                <i className="fas fa-envelope"></i>
+                <input type="email" placeholder="Email" required />
+              </div>
+              <button className="main-btn">Book Now</button>
             </div>
-            <button className="main-btn">Book Now</button>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
